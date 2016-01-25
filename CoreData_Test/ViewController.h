2fa24f93a26c9736bbb89fc39da63aa144c3b375
zhangdaliang <850086024@qq.com>
2016-01-25 14:26:03 +0800

    Initial Commit

---
+++ CoreData_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData_Test
+//
+//  Created by 张大亮 on 16/1/25.
+//  Copyright © 2016年 张大亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreData_Test
+//
+//  Created by 张大亮 on 16/1/25.
+//  Copyright © 2016年 张大亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
